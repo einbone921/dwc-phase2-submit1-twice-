c1045f049229effa234c5b89c8419acbaac2e314
einbone921 <einbone921@gmail.com>
2020-12-12 11:26:00 +0000

    Create new file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,13 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
+    @book_new = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
     else
       render :index
     end
